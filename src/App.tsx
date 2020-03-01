024dc183268f030a281c186f5b438c64d33d99b2
Jack McConnell <jackmc1122@gmail.com>
2020-03-01 11:48:50 -0800

    Add Material-UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Paper, Card, Typography } from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Paper>
+        <Card>
+          <Typography>Hello World!</Typography>
+        </Card>
+      </Paper>
     </div>
   );
 }
